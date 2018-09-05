02c006b8575f500180f480302da8ce1cea3f6c5a
Xin Liang <lx.altria@gmail.com>
2018-09-05 11:19:02 -0500

    update

---
+++ main_ts.cpp
@@ -0,0 +1,27 @@
+#include "compression.h"
+#include "utils.h"
+#include "time_based_compression.h"
+
+int main(int argc, char ** argv){
+
+    int status;
+    size_t nbEle;
+    // argv[1]: folder & var name
+    // e.g. /lcrc/project/ECP-EZ/public/compression/Hurricane-ISABEL/clean-data-dtzip/Cloudf
+    // argv[2-4] dimensions
+    int n1 = atoi(argv[3]);
+    int n2 = atoi(argv[4]);
+    int n3 = atoi(argv[5]);
+    // argv[6] snapshot_num
+    int snapshot_num = atoi(argv[6]);
+    // argv[7] interval
+    int interval = atoi(argv[7]);
+    // argv[8] eb in time/snapshot, may be separted later
+    double eb = atof(argv[8]);
+    size_t out_size = 0;
+    SZ_compression_in_time<float>(argv[1], snapshot_num, interval, eb, n1, n2, n3, &out_size);
+    // verify(data, dec_data, nbEle, out_size);
+    // free(comp_data);
+    // free(data);
+    // free(dec_data);
+}
