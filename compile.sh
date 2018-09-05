f3d5412b4e8fc62b98281f7e41c8d8b62ebbcbb2
Xin Liang <lx.altria@gmail.com>
2018-09-05 11:09:33 -0500

    update

--- compile.sh
+++ compile.sh
@@ -4,4 +4,5 @@ g++ -c tricubic_interpolation.cpp -o tci.o
 g++ -c trilinear_interpolation.cpp -o tli.o
 g++ -c sampling.cpp -o sample.o
 g++ main.cpp sample.o tci.o tli.o -o compression
+g++ main_ts.cpp sample.o tci.o tli.o -o compression_ts -I/Users/LiangXin/utils/sz_ts/include -L/Users/LiangXin/utils/sz_ts/lib -lSZ -lzlib -lzstd -lm
 
--- utils.h
+++ utils.h
@@ -25,6 +25,22 @@ Type * readfile(char * file, size_t * num){
 }
 
 template<typename Type>
+Type * readfile(char * file, size_t * num, Type * data){
+    std::ifstream fin(file, std::ios::binary);
+    if(!fin){
+        std::cout << " Error, Couldn't find the file" << "\n";
+        return 0;
+    }
+    fin.seekg(0, std::ios::end);
+    const size_t num_elements = fin.tellg() / sizeof(Type);
+    fin.seekg(0, std::ios::beg);
+    fin.read(reinterpret_cast<char*>(&data[0]), num_elements*sizeof(Type));
+    fin.close();
+    *num = num_elements;
+    return data;
+}
+
+template<typename Type>
 void writefile(char * file, Type * data, size_t num_elements){
 	std::ofstream fout(file, std::ios::binary);
 	fout.write(reinterpret_cast<const char*>(&data[0]), num_elements*sizeof(Type));
