4843026b4a30d9ce775046ecba3475f8c6576413
Xin Liang <lx.altria@gmail.com>
2018-09-19 13:36:46 -0500

    test

--- evaluate.py
+++ evaluate.py
@@ -61,13 +61,13 @@ def interpolation_in_space_1D(index, option, mode, executable="/home/xin/codes/t
 	interval_num = (83 - 1) // interval
 	actual_snapshot = interval_num * interval
 	sampling_dist = np.array([2, 4, 6, 8, 12, 16, 24, 32])
-	cr = np.zeros([sampling_dist.size, actual_snapshot])
+	br = np.zeros([sampling_dist.size, actual_snapshot])
 	psnr = np.zeros([sampling_dist.size, actual_snapshot])
 	nrmse = np.zeros([sampling_dist.size, actual_snapshot])
 	for i in range(sampling_dist.size):
 		os.system("{} {}/{} 1 1 1077290 83 1 1e-3 {} {} {}".format(executable, directory, var, sampling_dist[i], option, mode))
-		cr[i, :], psnr[i, :], nrmse[i, :] = get_statistics_1D("{}/{}".format(directory, var), 83, 1, modes[mode])
-	np.savetxt("{}_{}_{}_snapshot_cr.txt".format(var, option, mode), cr)
+		br[i, :], psnr[i, :], nrmse[i, :] = get_statistics_1D("{}/{}".format(directory, var), 83, 1, modes[mode])
+	np.savetxt("{}_{}_{}_snapshot_br.txt".format(var, option, mode), br)
 	np.savetxt("{}_{}_{}_snapshot_psnr.txt".format(var, option, mode), psnr)
 	np.savetxt("{}_{}_{}_snapshot_nrmse.txt".format(var, option, mode), nrmse)
 
@@ -80,13 +80,13 @@ def sz_space_1D(index, option=0, mode=0, executable="/home/xin/codes/test_compre
     interval_num = (83 - 1) // interval
     actual_snapshot = interval_num * interval
     sampling_dist = np.array(["1e-1", "1e-2", "1e-3", "1e-4", "1e-5"])
-    cr = np.zeros([sampling_dist.size, actual_snapshot])
+    br = np.zeros([sampling_dist.size, actual_snapshot])
     psnr = np.zeros([sampling_dist.size, actual_snapshot])
     nrmse = np.zeros([sampling_dist.size, actual_snapshot])
     for i in range(sampling_dist.size):
         os.system("{} {}/{} 1 1 1077290 83 1 {} 1 {} {}".format(executable, directory, var, sampling_dist[i], option, mode))
-        cr[i, :], psnr[i, :], nrmse[i, :] = get_statistics_1D("{}/{}".format(directory, var), 83, 1, modes[mode])
-    np.savetxt("{}_{}_{}_snapshot_cr.txt".format(var, option, mode), cr)
+        br[i, :], psnr[i, :], nrmse[i, :] = get_statistics_1D("{}/{}".format(directory, var), 83, 1, modes[mode])
+    np.savetxt("{}_{}_{}_snapshot_br.txt".format(var, option, mode), br)
     np.savetxt("{}_{}_{}_snapshot_psnr.txt".format(var, option, mode), psnr)
     np.savetxt("{}_{}_{}_snapshot_nrmse.txt".format(var, option, mode), nrmse)
 
@@ -96,8 +96,8 @@ def interpolation_in_time_1D(index, interval, mode, executable="/home/xin/codes/
 	modes = np.array(["szst", "szsdt", "dsszt", "dst"])
 	var = variables[index]
 	os.system("{} {}/{} 1 1 1077290 83 {} 0 1 0 {}".format(executable, directory, var, interval, mode))
-	cr, psnr, nrmse = get_statistics_1D("{}/{}".format(directory, var), 83, interval, modes[mode])
-	np.savetxt("{}_{}_time_interval_{}_cr.txt".format(var, modes[mode], interval), cr)
+	br, psnr, nrmse = get_statistics_1D("{}/{}".format(directory, var), 83, interval, modes[mode])
+	np.savetxt("{}_{}_time_interval_{}_br.txt".format(var, modes[mode], interval), br)
 	np.savetxt("{}_{}_time_interval_{}_psnr.txt".format(var, modes[mode], interval), psnr)
 	np.savetxt("{}_{}_time_interval_{}_nrmse.txt".format(var, modes[mode], interval), nrmse)
 
@@ -107,8 +107,8 @@ def sz_in_time_1D(index, eb, mode, executable="/home/xin/codes/test_compression/
 	modes = np.array(["szst", "szsdt", "dsszt", "dst"])
 	var = variables[index]
 	os.system("{} {}/{} 1 1 1077290 83 82 {} 1 0 {}".format(executable, directory, var, eb, mode))
-	cr, psnr, nrmse = get_statistics_1D("{}/{}".format(directory, var), 83, 82, modes[mode])
-	np.savetxt("{}_{}_time_eb_{}_cr.txt".format(var, modes[mode], eb), cr)
+	br, psnr, nrmse = get_statistics_1D("{}/{}".format(directory, var), 83, 82, modes[mode])
+	np.savetxt("{}_{}_time_eb_{}_br.txt".format(var, modes[mode], eb), br)
 	np.savetxt("{}_{}_time_eb_{}_psnr.txt".format(var, modes[mode], eb), psnr)
 	np.savetxt("{}_{}_time_eb_{}_nrmse.txt".format(var, modes[mode], eb), nrmse)
 
@@ -118,18 +118,58 @@ def interpolation_in_space_3D(index, option, mode, executable="/home/xin/codes/t
 	modes = np.array(["szst", "szsdt", "dsszt", "dst"])
 	var = variables[index]
 	interval = 1
-	interval_num = (83 - 1) // interval
+	interval_num = (48 - 1) // interval
 	actual_snapshot = interval_num * interval
 	sampling_dist = np.array([2, 3, 4, 5, 6])
-	cr = np.zeros([sampling_dist.size, actual_snapshot])
+	br = np.zeros([sampling_dist.size, actual_snapshot])
 	psnr = np.zeros([sampling_dist.size, actual_snapshot])
 	nrmse = np.zeros([sampling_dist.size, actual_snapshot])
 	total_br = np.zeros([sampling_dist.size])
 	total_psnr = np.zeros([sampling_dist.size])
 	for i in range(sampling_dist.size):
 		os.system("{} {}/{} 100 500 500 48 1 0 {} {} {}".format(executable, directory, var, sampling_dist[i], option, mode))
-		cr[i, :], psnr[i, :], nrmse[i, :] = get_statistics_3D("{}/{}".format(directory, var), 48, 1, modes[mode])
-	np.savetxt("{}_{}_{}_snapshot_cr.txt".format(var, option, mode), cr)
+		br[i, :], psnr[i, :], nrmse[i, :] = get_statistics_3D("{}/{}".format(directory, var), 48, 1, modes[mode])
+	np.savetxt("{}_{}_{}_snapshot_br.txt".format(var, option, mode), br)
 	np.savetxt("{}_{}_{}_snapshot_psnr.txt".format(var, option, mode), psnr)
 	np.savetxt("{}_{}_{}_snapshot_nrmse.txt".format(var, option, mode), nrmse)
 
+def sz_space_3D(index, option=0, mode=0, executable="/home/xin/codes/test_compression/compression_ts"):
+	directory = "/lcrc/project/ECP-EZ/public/compression/test_data/Hurricane"
+	variables = np.array(["QCLOUDf", "QGRAUPf", "QICEf", "QRAINf", "QSNOWf", "QVAPORf", "PRECIPf", "CLOUDf", "TCf", "Pf", "Uf", "Vf", "Wf"])
+    modes = np.array(["szst", "szsdt", "dsszt", "dst"])
+    var = variables[index]
+    interval = 1
+    interval_num = (48 - 1) // interval
+    actual_snapshot = interval_num * interval
+    sampling_dist = np.array(["1e-1", "1e-2", "1e-3", "1e-4", "1e-5"])
+    br = np.zeros([sampling_dist.size, actual_snapshot])
+    psnr = np.zeros([sampling_dist.size, actual_snapshot])
+    nrmse = np.zeros([sampling_dist.size, actual_snapshot])
+    for i in range(sampling_dist.size):
+        os.system("{} {}/{} 100 500 500 48 1 {} 1 {} {}".format(executable, directory, var, sampling_dist[i], option, mode))
+        br[i, :], psnr[i, :], nrmse[i, :] = get_statistics_3D("{}/{}".format(directory, var), 48, 1, modes[mode])
+    np.savetxt("{}_{}_{}_snapshot_br.txt".format(var, option, mode), br)
+    np.savetxt("{}_{}_{}_snapshot_psnr.txt".format(var, option, mode), psnr)
+    np.savetxt("{}_{}_{}_snapshot_nrmse.txt".format(var, option, mode), nrmse)
+
+def interpolation_in_time_3D(index, interval, mode=3, executable="/home/xin/codes/test_compression/compression_ts"):
+	directory = "/lcrc/project/ECP-EZ/public/compression/test_data/Hurricane"
+	variables = np.array(["QCLOUDf", "QGRAUPf", "QICEf", "QRAINf", "QSNOWf", "QVAPORf", "PRECIPf", "CLOUDf", "TCf", "Pf", "Uf", "Vf", "Wf"])
+	modes = np.array(["szst", "szsdt", "dsszt", "dst"])
+	var = variables[index]
+	os.system("{} {}/{} 100 500 500 48 {} 0 1 0 {}".format(executable, directory, var, interval, mode))
+	br, psnr, nrmse = get_statistics_3D("{}/{}".format(directory, var), 48, interval, modes[mode])
+	np.savetxt("{}_{}_time_interval_{}_br.txt".format(var, modes[mode], interval), br)
+	np.savetxt("{}_{}_time_interval_{}_psnr.txt".format(var, modes[mode], interval), psnr)
+	np.savetxt("{}_{}_time_interval_{}_nrmse.txt".format(var, modes[mode], interval), nrmse)
+
+def sz_in_time_3D(index, eb, mode=0, executable="/home/xin/codes/test_compression/compression_ts"):
+	directory = "/lcrc/project/ECP-EZ/public/compression/test_data/Hurricane"
+	variables = np.array(["QCLOUDf", "QGRAUPf", "QICEf", "QRAINf", "QSNOWf", "QVAPORf", "PRECIPf", "CLOUDf", "TCf", "Pf", "Uf", "Vf", "Wf"])
+	modes = np.array(["szst", "szsdt", "dsszt", "dst"])
+	var = variables[index]
+	os.system("{} {}/{} 100 500 500 48 47 {} 1 0 {}".format(executable, directory, var, eb, mode))
+	br, psnr, nrmse = get_statistics_3D("{}/{}".format(directory, var), 48, 47, modes[mode])
+	np.savetxt("{}_{}_time_eb_{}_br.txt".format(var, modes[mode], eb), br)
+	np.savetxt("{}_{}_time_eb_{}_psnr.txt".format(var, modes[mode], eb), psnr)
+	np.savetxt("{}_{}_time_eb_{}_nrmse.txt".format(var, modes[mode], eb), nrmse)
